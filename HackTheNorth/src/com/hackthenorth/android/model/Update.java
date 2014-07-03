b94c382b50232e602dcdc4c51a09a77650fb0d20
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2014-07-03 01:02:09 -0400

    Added firebase HTTP support, fixed formatting

--- HackTheNorth/src/com/hackthenorth/android/model/Update.java
+++ HackTheNorth/src/com/hackthenorth/android/model/Update.java
@@ -1,85 +1,99 @@
 package com.hackthenorth.android.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Update {
-	// Fields
-	public String title;
-	public String body;
-	
-	// JSON keys for fields
-	public static final String TITLE = "title";
-	public static final String BODY = "body";
-	
-	public Update(String json) {
-		JSONObject obj = null;
-		try {
-			obj = new JSONObject(json);
-			
-			// Note: When adding new fields, make sure to add them here!
+    
+    public String id;
+    
+    // Fields
+    public String title;
+    public String body;
+    
+    // JSON keys for fields
+    public static final String TITLE = "title";
+    public static final String BODY = "body";
+    
+    public Update(String id, String json) {
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(json);
+            
+            // Save ID
+            this.id = id;
+            
+            // Note: When adding new fields, make sure to add them here!
             title = obj.getString(TITLE);
             body = obj.getString(BODY);
-		} catch (JSONException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
-
-	public Update(JSONObject obj) {
-		try {
-			// Note: When adding new fields, make sure to add them here!
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public Update(JSONObject obj) {
+        try {
+            // Note: When adding new fields, make sure to add them here!
             title = obj.getString(TITLE);
             body = obj.getString(BODY);
-		} catch (JSONException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
-	
-	public JSONObject toJSON() {
-		JSONObject obj = new JSONObject();
-		try {
-			// Note: When adding new fields, make sure to add them here!
-			obj.put(BODY, body);
-			obj.put(TITLE, title);
-		} catch (JSONException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		return obj;
-	}
-	
-	public static ArrayList<Update> loadUpdateArrayFromJSON(String json) {
-		
-		// Open the JSON array from string
-		JSONArray arr;
-		try {
-			arr = new JSONArray(json);
-		} catch (JSONException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-
-		ArrayList<Update> updates = new ArrayList<Update>(arr.length());
-		for (int i = 0; i < arr.length(); i++) {
-			// Get the ith JSON object out of the array
-			JSONObject obj;
-			try {
-				obj = arr.getJSONObject(i);
-			} catch (JSONException e) {
-				e.printStackTrace();
-				throw new RuntimeException(e);
-			}
-			
-			// Load the update from that object and add it to the result
-			Update update = new Update(obj);
-			updates.add(update);
-		}
-		
-		return updates;
-	}
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static ArrayList<Update> loadUpdateArrayFromJSON(String json) {
+        
+        // Open the JSON object from string
+        JSONObject obj;
+        try {
+            obj = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        
+        ArrayList<Update> updates = new ArrayList<Update>(obj.length());
+        
+        // Note: The JSONObject API returns an Interator<String>, so this is a
+        // safe cast.
+        @SuppressWarnings("unchecked")
+        Iterator<String> it = obj.keys();
+        
+        while (it.hasNext()) {
+            // Get the key from the JSON object.
+            String id = it.next();
+            
+            JSONObject updateJSON;
+            try {
+                updateJSON = obj.getJSONObject(id);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+            
+            // Load the update from that object and add it to the result
+            Update update = new Update(updateJSON);
+            update.id = id;
+            updates.add(update);
+        }
+        
+        // Sort the list
+        Collections.sort(updates, new Comparator<Update>() {
+            @Override
+            public int compare(Update lhs, Update rhs) {
+                // Sort them in descending order---that's why rhs is on the lhs
+                // in this expression.
+                return rhs.id.compareTo(lhs.id);
+            }
+        });
+        
+        return updates;
+    }
 }
--- HackTheNorth/src/com/hackthenorth/android/view/NavigationDrawerFragment.java
+++ HackTheNorth/src/com/hackthenorth/android/view/NavigationDrawerFragment.java
@@ -25,121 +25,119 @@ import android.widget.Toast;
 import com.hackthenorth.android.R;
 
 /**
- * Fragment used for managing interactions for and presentation of a navigation drawer.
- * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
- * design guidelines</a> for a complete explanation of the behaviors implemented here.
+ * Fragment used for managing interactions for and presentation of a navigation
+ * drawer. See the <a href=
+ * "https://developer.android.com/design/patterns/navigation-drawer.html#Interaction"
+ * > design guidelines</a> for a complete explanation of the behaviors
+ * implemented here.
  */
 public class NavigationDrawerFragment extends Fragment {
-
+    
     /**
      * Remember the position of the selected item.
      */
     private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
-
+    
     /**
-     * Per the design guidelines, you should show the drawer on launch until the user manually
-     * expands it. This shared preference tracks this.
+     * Per the design guidelines, you should show the drawer on launch until the
+     * user manually expands it. This shared preference tracks this.
      */
     private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
-
+    
     /**
      * A pointer to the current callbacks instance (the Activity).
      */
     private NavigationDrawerCallbacks mCallbacks;
-
+    
     /**
      * Helper component that ties the action bar to the navigation drawer.
      */
     private ActionBarDrawerToggle mDrawerToggle;
-
+    
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerListView;
     private View mFragmentContainerView;
-
+    
     private int mCurrentSelectedPosition = 0;
     private boolean mFromSavedInstanceState;
     private boolean mUserLearnedDrawer;
-
+    
     public NavigationDrawerFragment() {
     }
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        
         // Read in the flag indicating whether or not the user has demonstrated awareness of the
         // drawer. See PREF_USER_LEARNED_DRAWER for details.
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
         mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
-
+        
         if (savedInstanceState != null) {
             mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
             mFromSavedInstanceState = true;
         }
-
+        
         // Select either the default item (0) or the last selected item.
         selectItem(mCurrentSelectedPosition);
     }
-
+    
     @Override
-    public void onActivityCreated (Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        // Indicate that this fragment would like to influence the set of actions in the action bar.
+        // Indicate that this fragment would like to influence the set of actions in the 
+        // action bar.
         setHasOptionsMenu(true);
     }
-
+    
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        mDrawerListView = (ListView) inflater.inflate(
-                R.layout.fragment_navigation_drawer, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mDrawerListView = (ListView) inflater.inflate(R.layout.fragment_navigation_drawer,
+                container, false);
         mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 selectItem(position);
             }
         });
-        mDrawerListView.setAdapter(new ArrayAdapter<String>(
-                getActionBar().getThemedContext(),
-                android.R.layout.simple_list_item_activated_1,
-                android.R.id.text1,
-                new String[]{
-                        getString(R.string.section_updates),
-                }));
+        mDrawerListView.setAdapter(new ArrayAdapter<String>(getActionBar().getThemedContext(),
+                android.R.layout.simple_list_item_activated_1, android.R.id.text1,
+                new String[] { getString(R.string.section_updates), }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
     }
-
+    
     public boolean isDrawerOpen() {
         return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(mFragmentContainerView);
     }
-
+    
     /**
-     * Users of this fragment must call this method to set up the navigation drawer interactions.
-     *
-     * @param fragmentId   The android:id of this fragment in its activity's layout.
+     * Users of this fragment must call this method to set up the navigation
+     * drawer interactions.
+     * 
+     * @param fragmentId The android:id of this fragment in its activity's layout.
      * @param drawerLayout The DrawerLayout containing this fragment's UI.
      */
     public void setUp(int fragmentId, DrawerLayout drawerLayout) {
         mFragmentContainerView = getActivity().findViewById(fragmentId);
         mDrawerLayout = drawerLayout;
-
+        
         // set a custom shadow that overlays the main content when the drawer opens
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        // set up the drawer's list view with items and click listener
 
+        // set up the drawer's list view with items and click listener
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeButtonEnabled(true);
-
+        
         // ActionBarDrawerToggle ties together the the proper interactions
         // between the navigation drawer and the action bar app icon.
-        mDrawerToggle = new ActionBarDrawerToggle(
-                getActivity(),                    /* host Activity */
-                mDrawerLayout,                    /* DrawerLayout object */
-                R.drawable.ic_drawer,             /* nav drawer image to replace 'Up' caret */
-                R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
-                R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), /* host Activity */
+        mDrawerLayout, /* DrawerLayout object */
+        R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+        R.string.navigation_drawer_open, /* "open drawer" description for accessibility */
+        R.string.navigation_drawer_close /* "close drawer" description for accessibility */
         ) {
             @Override
             public void onDrawerClosed(View drawerView) {
@@ -147,17 +145,17 @@ public class NavigationDrawerFragment extends Fragment {
                 if (!isAdded()) {
                     return;
                 }
-
+                
                 getActivity().supportInvalidateOptionsMenu(); // calls onPrepareOptionsMenu()
             }
-
+            
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
                 if (!isAdded()) {
                     return;
                 }
-
+                
                 if (!mUserLearnedDrawer) {
                     // The user manually opened the drawer; store this flag to prevent auto-showing
                     // the navigation drawer automatically in the future.
@@ -166,17 +164,17 @@ public class NavigationDrawerFragment extends Fragment {
                             .getDefaultSharedPreferences(getActivity());
                     sp.edit().putBoolean(PREF_USER_LEARNED_DRAWER, true).apply();
                 }
-
+                
                 getActivity().supportInvalidateOptionsMenu(); // calls onPrepareOptionsMenu()
             }
         };
-
+        
         // If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
         // per the navigation drawer design guidelines.
         if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
             mDrawerLayout.openDrawer(mFragmentContainerView);
         }
-
+        
         // Defer code dependent on restoration of previous instance state.
         mDrawerLayout.post(new Runnable() {
             @Override
@@ -184,10 +182,10 @@ public class NavigationDrawerFragment extends Fragment {
                 mDrawerToggle.syncState();
             }
         });
-
+        
         mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
-
+    
     private void selectItem(int position) {
         mCurrentSelectedPosition = position;
         if (mDrawerListView != null) {
@@ -200,7 +198,7 @@ public class NavigationDrawerFragment extends Fragment {
             mCallbacks.onNavigationDrawerItemSelected(position);
         }
     }
-
+    
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -210,26 +208,26 @@ public class NavigationDrawerFragment extends Fragment {
             throw new ClassCastException("Activity must implement NavigationDrawerCallbacks.");
         }
     }
-
+    
     @Override
     public void onDetach() {
         super.onDetach();
         mCallbacks = null;
     }
-
+    
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(STATE_SELECTED_POSITION, mCurrentSelectedPosition);
     }
-
+    
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         // Forward the new configuration the drawer toggle component.
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
-
+    
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // If the drawer is open, show the global app actions in the action bar. See also
@@ -240,24 +238,25 @@ public class NavigationDrawerFragment extends Fragment {
         }
         super.onCreateOptionsMenu(menu, inflater);
     }
-
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
+        
         if (item.getItemId() == R.id.action_example) {
             Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
-
+        
         return super.onOptionsItemSelected(item);
     }
-
+    
     /**
-     * Per the navigation drawer design guidelines, updates the action bar to show the global app
-     * 'context', rather than just what's in the current screen.
+     * Per the navigation drawer design guidelines, updates the action bar to
+     * show the global app 'context', rather than just what's in the current
+     * screen.
      */
     private void showGlobalContextActionBar() {
         ActionBar actionBar = getActionBar();
@@ -265,13 +264,14 @@ public class NavigationDrawerFragment extends Fragment {
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setTitle(R.string.app_name);
     }
-
+    
     private ActionBar getActionBar() {
         return ((ActionBarActivity) getActivity()).getSupportActionBar();
     }
-
+    
     /**
-     * Callbacks interface that all activities using this fragment must implement.
+     * Callbacks interface that all activities using this fragment must
+     * implement.
      */
     public static interface NavigationDrawerCallbacks {
         /**
