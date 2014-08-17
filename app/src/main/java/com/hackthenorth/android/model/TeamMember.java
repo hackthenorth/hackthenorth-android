75e03279c096a03e62f13a7af5bf5ce193a3306c
Moez Bhatti <moez.bhatti@gmail.com>
2014-08-16 21:37:01 -0400

    Created TeamMember model

---
+++ app/src/main/java/com/hackthenorth/android/model/TeamMember.java
@@ -0,0 +1,53 @@
+package com.hackthenorth.android.model;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class TeamMember {
+    private static final String TAG = "TeamMember";
+
+    public String id;
+
+    // Fields
+    public String name;
+    public ArrayList<String> role;
+    public String Phone;
+    public String email;
+    public String twitter;
+
+    public TeamMember() {
+    }
+
+    // This method takes a JSON string and returns an ArrayList of TeamMember from
+    // the JSON data.
+    public static ArrayList<TeamMember> loadTeamMemberArrayFromJSON(String json) {
+
+        // Deserialize using GSON.
+        Gson gson = new Gson();
+        Type type = new TypeToken<HashMap<String, TeamMember>>(){}.getType();
+        HashMap<String, TeamMember> teamMemberMap = gson.fromJson(json, type);
+
+        // Build an ArrayList of teamMembers from the hashmap.
+        ArrayList<TeamMember> teamMembers = new ArrayList<TeamMember>(teamMemberMap.size());
+        for (Map.Entry<String, TeamMember> entry : teamMemberMap.entrySet()) {
+            TeamMember teamMember = entry.getValue();
+            teamMember.id = entry.getKey();
+            teamMembers.add(teamMember);
+        }
+
+        // Sort the list
+        Collections.sort(teamMembers, new Comparator<TeamMember>() {
+            @Override
+            public int compare(TeamMember lhs, TeamMember rhs) {
+                // Sort them in descending order---that's why rhs is on the lhs
+                // in this expression.
+                return rhs.id.compareTo(lhs.id);
+            }
+        });
+
+        return teamMembers;
+    }
+}
