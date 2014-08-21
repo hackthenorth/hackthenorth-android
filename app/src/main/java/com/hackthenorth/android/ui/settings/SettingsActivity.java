8fb7498e700492484a96a52676c1364bf7793363
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2014-08-21 04:21:06 -0400

    made changes re: guidelines.

--- app/src/main/java/com/hackthenorth/android/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/hackthenorth/android/ui/settings/SettingsActivity.java
@@ -1,11 +1,19 @@
 package com.hackthenorth.android.ui.settings;
 
+import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.hackthenorth.android.R;
+import com.hackthenorth.android.ui.component.TextView;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -15,8 +23,29 @@ public class SettingsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Gives the back button in the action bar
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        Resources resources = getResources();
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.titleview, null);
+
+        TextView title = (TextView)view.findViewById(R.id.title);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/bebas_neue.ttf");
+        title.setTypeface(tf);
+        title.setText(resources.getString(R.string.settings_activity).toUpperCase());
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setCustomView(view);
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintColor(resources.getColor(R.color.theme_primary));
+        tintManager.setNavigationBarTintEnabled(true);
+        tintManager.setNavigationBarTintColor(resources.getColor(R.color.theme_primary));
 
         Fragment settingsFragment = new SettingsFragment();
         getFragmentManager().beginTransaction()
