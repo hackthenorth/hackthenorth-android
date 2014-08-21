646da75ed5be592344550700a8fa7e806be063d9
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2014-08-21 04:21:06 -0400

    made changes re: guidelines.

--- app/src/main/java/com/hackthenorth/android/base/BaseActivity.java
+++ app/src/main/java/com/hackthenorth/android/base/BaseActivity.java
@@ -1,9 +1,19 @@
 package com.hackthenorth.android.base;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.hackthenorth.android.HackTheNorthApplication;
+import com.hackthenorth.android.R;
 import com.hackthenorth.android.framework.VisibilityManager;
+import com.hackthenorth.android.ui.component.TextView;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * A base class for activities. This is used to determine if our app is in the foreground.
@@ -15,6 +25,37 @@ import com.hackthenorth.android.framework.VisibilityManager;
  */
 public class BaseActivity extends Activity {
 
+    protected TextView mTitle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Resources resources = getResources();
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.titleview, null);
+
+        mTitle = (TextView) view.findViewById(R.id.title);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/bebas_neue.ttf");
+        mTitle.setTypeface(tf);
+        mTitle.setText(resources.getString(R.string.app_name).toUpperCase());
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setCustomView(view);
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintColor(resources.getColor(R.color.theme_primary));
+        tintManager.setNavigationBarTintEnabled(true);
+        tintManager.setNavigationBarTintColor(resources.getColor(R.color.theme_primary));
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -25,5 +66,11 @@ public class BaseActivity extends Activity {
     public void onPause() {
         super.onPause();
         VisibilityManager.activityPaused();
+    }
+
+    @Override
+    public void setTitle(CharSequence chars) {
+        mTitle.setText(chars);
+        super.setTitle(chars);
     }
 }
--- app/src/main/java/com/hackthenorth/android/ui/MainActivity.java
+++ app/src/main/java/com/hackthenorth/android/ui/MainActivity.java
@@ -1,16 +1,11 @@
 package com.hackthenorth.android.ui;
 
-import android.app.ActionBar;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.devspark.robototextview.util.RobotoTypefaceManager;
@@ -19,21 +14,13 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.hackthenorth.android.R;
 import com.hackthenorth.android.base.BaseActivity;
 import com.hackthenorth.android.framework.GCMRegistrationManager;
-import com.hackthenorth.android.framework.VisibilityManager;
 import com.hackthenorth.android.ui.component.PagerTitleStrip;
-import com.hackthenorth.android.ui.component.TextView;
 import com.hackthenorth.android.ui.settings.SettingsActivity;
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class MainActivity extends BaseActivity {
 
     private static final String TAG = "MainActivity";
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-
-    private Resources mResources;
-    private ActionBar mActionBar;
-    private LayoutInflater mInflater;
-    private TextView mTitle;
 
     private ViewPagerAdapter mViewPagerAdapter;
     private PagerTitleStrip mViewPagerTabs;
@@ -45,26 +32,6 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         mViewPagerAdapter = new ViewPagerAdapter(this, getFragmentManager());
-
-        mResources = getResources();
-
-        mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = mInflater.inflate(R.layout.titleview, null);
-        mTitle = (TextView) view.findViewById(R.id.title);
-        mTitle.setText(getTitle());
-
-        mActionBar = getActionBar();
-        mActionBar.setDisplayHomeAsUpEnabled(false);
-        mActionBar.setDisplayShowCustomEnabled(true);
-        mActionBar.setDisplayShowHomeEnabled(false);
-        mActionBar.setDisplayShowTitleEnabled(false);
-        mActionBar.setCustomView(view);
-
-        SystemBarTintManager tintManager = new SystemBarTintManager(this);
-        tintManager.setStatusBarTintEnabled(true);
-        tintManager.setStatusBarTintColor(mResources.getColor(R.color.theme_primary));
-        tintManager.setNavigationBarTintEnabled(true);
-        tintManager.setNavigationBarTintColor(mResources.getColor(R.color.theme_primary));
 
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mViewPagerAdapter);
@@ -86,13 +53,6 @@ public class MainActivity extends BaseActivity {
     public void onResume() {
         super.onResume();
         checkPlayServices();
-        VisibilityManager.activityResumed();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        VisibilityManager.activityPaused();
     }
 
     @Override
@@ -114,12 +74,6 @@ public class MainActivity extends BaseActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void setTitle(CharSequence chars) {
-        mTitle.setText(chars);
-        super.setTitle(chars);
     }
 
     // Copied from the GCM Client tutorial [1].
